a305f7d98f9e7bd39d62dacd481e5ee2a1e1aa14
francisshere <francisadriangapol18@gmail.com>
2026-02-07 01:55:41 +0800

    done 5th test

--- foundations/06_repeatString/repeatString.spec.js
+++ foundations/06_repeatString/repeatString.spec.js
@@ -13,7 +13,7 @@ describe('repeatString', () => {
   test('repeats the string 0 times', () => {
     expect(repeatString('bye', 0)).toEqual('');
   });
-  test.skip('handles any non-negative integer for repeat count', function () {
+  test('handles any non-negative integer for repeat count', function () {
     /* This test ensures your function is "generic", meaning the previous
       tests were not completed by hardcoding values for their respective numbers */
     const number = Math.floor(Math.random() * 1000);
