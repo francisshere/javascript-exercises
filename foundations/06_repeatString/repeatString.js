c04f55a813029f6bb92c87d5155a5ae0d4549679
francisshere <francisadriangapol18@gmail.com>
2026-02-07 01:49:02 +0800

    done first test :))

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let concat = string;
 
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i=1; i < num; i++){
+            concat += string;
+        }
+        return concat;
+    }
+   
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
