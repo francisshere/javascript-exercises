6aec5b353a482e1b13b7bfa7f5781c114e9ff4b9
francisshere <francisadriangapol18@gmail.com>
2026-02-11 00:08:44 +0800

    done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // split the string into an array of characters
+    // reverse the array
+    // join the array back into a string
+    return str.split("").reverse().join("");        
 
-};
+}
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
